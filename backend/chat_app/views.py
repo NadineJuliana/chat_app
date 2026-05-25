300fa28b4d6ef985d320c4e5b477c5cddcb6ba8b
NadineJuliana <nadine.juliana.b@web.de>
2026-05-26 01:31:16 +0200

    Add chat_view function with Get/Post data

--- backend/chat_app/views.py
+++ backend/chat_app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Chat
+from django.http import JsonResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+@csrf_exempt
+def chat_view(request):
+    if request.method == "GET":
+
+        chats = Chat.objects.all().values()
+
+        return JsonResponse(list(chats), safe=False)
+
+    elif request.method == "POST":
+
+        data = json.loads(request.body)
+
+        chat = Chat.objects.create(
+            name=data["name"],
+            message=data["message"]
+        )
+
+        return JsonResponse({
+            "message": "Message created successfully",
+        })
