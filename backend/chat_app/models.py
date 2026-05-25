ebcf5b810775a87fa2fa5b47e4b1603da8245df8
NadineJuliana <nadine.juliana.b@web.de>
2026-05-26 01:29:46 +0200

    Add class Chat on models.py

--- backend/chat_app/models.py
+++ backend/chat_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Chat(models.Model):
+    name = models.CharField(max_length=100)
+    message = models.TextField(default="")
+    created_at = models.DateTimeField(auto_now_add=True)
